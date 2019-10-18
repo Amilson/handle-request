81c03bf6b31d718801b338ceb4847d3671e97128
Amilson <amilson.mfontes@hotmail.com>
2019-10-17 22:47:25 -0300

    Enviroment configuration

--- api/router/handle-param-body-request.ts
+++ api/router/handle-param-body-request.ts
@@ -4,7 +4,7 @@ import { regExpUrlTesting, persistNullEmptyUndefined, regExpNumberTesting } from
 export const handleParamBodyRequest = async(ctx: BaseContext, next: () => Promise<any>) => {
     try {
         let { delay, protocol, url, error } = ctx.params;
-        protocol = protocol.replace(/[^A-Za-z]/g,'');
+        
         let errorMessage: string[] = [];
         if(persistNullEmptyUndefined(delay) && !regExpNumberTesting.test(delay))
             errorMessage.push(`Param [delay] must contain only numbers. Expected [0-9], received [${delay}].`);
@@ -12,8 +12,12 @@ export const handleParamBodyRequest = async(ctx: BaseContext, next: () => Promis
         if(persistNullEmptyUndefined(error) && !regExpNumberTesting.test(error))
             errorMessage.push(`Param [error] must contain only numbers. Expected [0-9], received [${delay}].`);
         
-        if(persistNullEmptyUndefined(protocol) && !(protocol === 'http' || protocol === 'https'))
-            errorMessage.push(`Param [url] must contain protocol http or https`);
+        if(persistNullEmptyUndefined(protocol)){
+            protocol = protocol.replace(/[^A-Za-z]/g,'');
+            if(!(protocol === 'http' || protocol === 'https')){
+                errorMessage.push(`Param [url] must contain protocol http or https`);
+            }
+        }            
 
         if(persistNullEmptyUndefined(url) && !regExpUrlTesting.test(url))
             errorMessage.push(`Param [url] is invalid`);
